28cf3d3413d92330991387bf94e45dcf58b7879f
SurabhiYadav1 <s.yadav@gembainfotech.com>
2025-04-16 00:03:29 +0530

    new build added

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import { CgGitFork } from "react-icons/cg";
 import { IoStar } from "react-icons/io5";
 
-
 function Footer() {
   return (
     <div className="relative border-t bg-[#0d1224] border-[#353951] text-white">
